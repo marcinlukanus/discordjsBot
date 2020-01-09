3a68f090f5efca4fdd67628622f3a1044dc8308a
Marcin <mlukanus@gmail.com>
2020-01-08 22:41:10 -0600

    Adds a few simple message checks for the bot to read and response to

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const { prefix } = require('./config.json')
 
 client.once('ready', () => {
 	console.log('Ready!');
+});
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+    // const command = args.shift().toLowerCase();
+    
+	if (message.content === `${prefix}ping`) {
+        message.channel.send('pong');
+    } else if (message.content === `${prefix}server`) {
+        message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+    } else if (message.content === `${prefix}pong`) {
+        message.channel.send('ping');
+    }
 });
 
 client.login(process.env.DISCORDJS_TOKEN);
