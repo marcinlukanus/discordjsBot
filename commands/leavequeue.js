d6e61e47855e030f6c3b248fe96c26f577f3d3ba
Marcin <mlukanus@gmail.com>
2020-01-12 16:22:45 -0600

    Adds command to allow user to leave queue

---
+++ commands/leavequeue.js
@@ -0,0 +1,19 @@
+import queue from '../index'
+
+module.exports = {
+	name: 'leavequeue',
+    description: 'Allows users to leave 6mans queue',
+    aliases: ['l', 'leave'],
+	execute(message) {
+        // Check to make sure user is not in queue
+        if (!queue.includes(message.author)) {
+            return message.channel.send('User not already in queue!');
+        }
+
+        console.log('Queue: ' + queue);
+        console.log('Author: ' + message.author);
+
+        queue.pop(message.author);
+        message.channel.send(message.author + ' has left the queue! There are currently ' + queue.length + ' people in the queue.');
+	},
+};
