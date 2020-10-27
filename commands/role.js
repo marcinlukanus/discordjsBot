d3adee352153eb3dab9b559276643bf9208a892e
Marcin Lukanus <mlukanus@gmail.com>
2020-10-26 19:50:51 -0500

    Prevents Peabs from adding 'Alumni' role (#1)
    * no alumni for peabs
    
    * Fixes Jared's code
    
    Co-authored-by: jhermon <jhermon@iastate.edu>

--- commands/role.js
+++ commands/role.js
@@ -1,29 +1,33 @@
 /* eslint-disable no-useless-escape */
 module.exports = {
-	name: 'role',
-	description: 'Grants user with a role',
+    name: 'role',
+    description: 'Grants user with a role',
     guildOnly: true,
     cooldown: 5,
-	execute(message, args) {
+    execute(message, args) {
         const invalidRole = 'Please select a valid role in the format \`!role <status>\`: \`student\`, \`prospective\`, \`alumni\`, \`6mans\`, or \`imposter\`!';
 
-		if (!args[0]) {
-			return message.reply(invalidRole);
+        if (!args[0]) {
+            return message.reply(invalidRole);
         }
-        
+
         const name = args[0].toLowerCase();
         let roleName;
 
         if (name === 'student') {
-            roleName = message.guild.roles.find(role => role.name === "Students");
+            roleName = message.guild.roles.find(role => role.name === 'Students');
         } else if (name === 'prospective') {
-            roleName = message.guild.roles.find(role => role.name === "Prospective Students");
+            roleName = message.guild.roles.find(role => role.name === 'Prospective Students');
         } else if (name === 'alumni') {
-            roleName = message.guild.roles.find(role => role.name === "Alumni");
+            if (message.author.id === '246801277873422338') {
+                return message.channel.send('https://giphy.com/embed/UK5AQccKV9OMg');
+            } else {
+                roleName = message.guild.roles.find(role => role.name === 'Alumni');
+            }
         } else if (name === 'imposter') {
-            roleName = message.guild.roles.find(role => role.name === "Imposter");
+            roleName = message.guild.roles.find(role => role.name === 'Imposter');
         } else if (name === '6mans') {
-            roleName = message.guild.roles.find(role => role.name === "6mans");
+            roleName = message.guild.roles.find(role => role.name === '6mans');
         } else {
             return message.channel.send(invalidRole);
         }
@@ -33,5 +37,5 @@ module.exports = {
             .catch(console.error);
 
         message.channel.send(`You have been given the \`${roleName.name}\` role!`);
-	},
+    },
 };
